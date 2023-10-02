9a7fbfcde1b663d31130ad09891f67ce9c40dcec
Lahirerr <m_tanachart@hotmail.com>
2023-10-02 15:50:46 +0700

    savepoint1

---
+++ src/components/Firstsection.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div class="bg-black">
+<div class="flex items-center justify-center h-screen flex-col">
+  <h1 class="text-[164px] font-modern text-white ">ม.๑๑๒</h1>
+  <p class="text-white text-[18px]">“ผู้ใดหมิ่นประมาท ดูหมิ่น หรือแสดงความอาฆาตมาดร้าย พระมหากษัตริย์ พระราชินี รัชทายาท
+    </p>
+    <p class="text-white text-[18px]">หรือผู้สำเร็จราชการแทนพระองค์ ต้องระวางโทษจำคุกตั้งแต่สามปีถึงสิบห้าปี”</p>
+</div>
+    </div>
+
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ src/components/Secondsection.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+ 
+<template>
+    <div class="bg-white">
+        <div class="flex items-center h-screen flex-col">
+            <div class="flex flex-col pt-20 font-modern">
+                <h1 class="text-7xl font-modern text-black font-bold">จำนวณคดีที่เกิดขึ้น</h1>
+                <p class="text-2xl text-black">มาตรา 112 มีการตีความการกระทำอย่างไร้ขอบเขต แต่ทุกคนสามารถผู้กล่าวโทษให้ดำเนินคดีได้ 
+                <br>จึงมีการกล่าวหากันเป็นจำนวนมากในช่วงที่มีความขัดแย้งทางการเมืองหลังจากปี 2548 เป็นต้นมา
+            </p>
+            </div>
+
+            
+            
+        </div>
+    </div>
+</template>
+ 
+<style scoped></style>
