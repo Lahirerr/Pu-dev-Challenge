f484f38c3705a1b76c06ee5d0544e9c9ec6fb255
Lahirerr <m_tanachart@hotmail.com>
2023-10-04 01:44:15 +0700

    responsive section three

--- src/components/Thirdsection.vue
+++ src/components/Thirdsection.vue
@@ -4,59 +4,59 @@
   
   <template>
     <div class="bg-black w-full h-full text-white flex justify-center"> 
-      <div class="w-3/4 p-7 text-left space-y-10 flex flex-col"> 
-        <div class="w-2/4 self-start"> 
-          <p class="font-bold font-modern text-6xl">ความเห็นนักวิชาการ</p>
-          <p class="text-2xl font-modern pt-10">
+      <div class="xl:w-3/4 p-7 text-left space-y-10 flex flex-col"> 
+        <p class="font-bold font-modern xl:text-6xl text-5xl text-center">ความเห็นนักวิชาการ</p>
+        <div class="xl:w-2/4 xl:self-start xl:text-left text-center"> 
+          <p class="xl:text-2xl text-xl font-modern pt-10">
             “ปัญหาใหญ่มากๆ ของมาตรา 112 คือมันมีนโยบายที่ให้ใช้อย่างร้ายแรงที่สุด ให้เป็นความลับ มันทำให้ภาพลักษณ์ของระบบยุติธรรมในคดี 112 
             เป็นกฎหมายที่ไม่ชัดเจนแน่นอน กฎหมายไม่มีความโปร่งใส และไม่สมเหตุสมผล  
             ไม่ว่าต่อไปนี้คุณจะตัดสินคดีอื่น คดีไหนก็ตาม คนก็จะเกิดความไม่เชื่อถือ 
             เกิดความสงสัยในการกระทำของสถาบันตุลาการ ถ้าเป็นอย่างนั้นสังคมไทยอยู่ไม่ได้”
           </p>
-          <div class="flex flex-row pt-5">
-            <div>
+          
+          <div class="flex xl:flex-row flex-col pt-5 text-center">
+            <div class="flex justify-center">
               <img src="/person-1.png" alt="" width="100px">
             </div>
-            <div class="flex flex-col justify-center pl-5 font-modern text-2xl">
+            <div class="flex flex-col justify-center pl-5 font-modern xl:text-2xl text-xl">
               <p class="font-bold">ดร. เข็มทอง ต้นสกุลรุ่งเรือง</p>
               <p>คณะนิติศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย</p>
             </div>   
           </div>
         </div>
         
-        <div class="w-2/4 text-right self-end flex flex-col"> 
-            <p class="text-2xl font-modern">
+        <div class="xl:w-2/4 xl:text-right xl:self-end flex flex-col text-center"> 
+            <p class="xl:text-2xl text-xl font-modern">
             “เราสามารถมีระบอบประชาธิปไตยอันมีพระมหากษัตริย์ทรงเป็นประมุขได้ 
             แต่เราก็สามารถทำให้กฏหมายนั้น มันไม่ขัดแย้งต่อหลักการพื้นฐาน สิทธิ 
             และเสรีภาพของประชาชนในสังคมประชาธิปไตยได้ และนั่นก็ควรเป็นสิ่งที่ควรจะทำ”
           </p>
-          <div class="flex flex-row pt-5 space-x-4 font-modern text-2xl self-end">
-            <div class="flex flex-col justify-center">
-              <p class="font-bold">รองศาสตราจารย์ ดร. วาสนา วงศ์สุรวัฒน์</p>
-              <p>คณะอักษรศาสตร์ ภาควิชาประวัติศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย</p>
-            </div>   
-            <div>
+          <div class="flex xl:flex-row pt-5 font-modern text-2xl xl:self-end flex-col">
+            <div class="xl:order-last flex justify-center">
               <img src="/person-2.png" alt="" width="100px">
             </div>
+            <div class="flex flex-col justify-center pr-5 xl:text-2xl text-xl xl:pt-0 pt-2">
+              <p class="font-bold">รองศาสตราจารย์ ดร. วาสนา วงศ์สุรวัฒน์</p>
+              <p>คณะอักษรศาสตร์ ภาควิชาประวัติศาสตร์ <span class="inline-block xl:block">จุฬาลงกรณ์มหาวิทยาลัย</span></p>
+            </div>  
           </div>
         </div>
 
-        <div class="w-2/4 text-left self-start flex flex-col"> 
-            <p class="text-2xl font-modern">
+        <div class="xl:w-2/4 xl:text-left text-center xl:self-start flex flex-col"> 
+            <p class="xl:text-2xl text-xl font-modern">
                 “ควรมีบุคคลหรือองค์กรในระดับสูง ซึ่งไม่เกี่ยวกับพระมหากษัตริย์โดยทางหนึ่งทางใด อาจเป็นองค์กรที่มีอยู่แล้วเช่น 
                 อัยการสูงสุด รัฐมนตรีกระทรวงวัฒนธรรม นายกรัฐมนตรี หรือตั้งองค์กรใหม่ขึ้นกลั่นกรองก่อนจะอนุมัติให้ดำเนินคดีได้ 
                 อย่าปล่อยให้ใครๆ ก็สามารถตั้งตัวเป็นโจทย์ฟ้องร้องได้ 
                 และปล่อยให้ตำรวจชั้นผู้น้อยและอัยการชั้นผู้น้อย ต้องใช้วินิจฉัยของตนเองว่าจะดำเนินคดีหรือไม่ ซึ่งยากที่จะใช้อำนาจวินิจฉัยนั้นอย่างเที่ยงธรรม”
           </p>
-          <div class="flex flex-row pt-5 space-x-4 font-modern text-2xl self-start">
-            <div>
+          <div class="flex xl:flex-row pt-5 space-x-4 font-modern xl:text-2xl xl:self-start flex-col">
+            <div class="flex justify-center">
               <img src="/person-3.png" alt="" width="100px">
             </div>
-            <div class="flex flex-col justify-center">
+            <div class="flex flex-col justify-center xl:text-2xl text-xl">
               <p class="font-bold">นิธิ เอียวศรีวงศ์</p>
-              <p>อดีตอาจารย์ประจำภาควิชาประวัติศาสตร์ คณะมนุษยศาสตร์ มหาวิทยาลัยเชียงใหม่</p>
+              <p>อดีตอาจารย์ประจำ ภาควิชาประวัติศาสตร์ <span class="inline-block xl:block">คณะมนุษยศาสตร์ มหาวิทยาลัยเชียงใหม่</span></p>
             </div>   
-            
           </div>
         </div>
       </div>
